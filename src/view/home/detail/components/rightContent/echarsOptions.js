e9146de6e109947387b8fec0602324804022e094
Cocoa.kk <kk1650213306@163.com>
2025-06-08 16:35:00 +0800

    feat: add rightContent component for personnel control, safety supervision, and quality monitoring with charts and data display

--- src/view/home/detail/components/rightContent/echarsOptions.js
+++ src/view/home/detail/components/rightContent/echarsOptions.js
@@ -371,7 +371,7 @@ export const coverageData4=ref({
     {
       name: 'Nightingale Chart',
       type: 'pie',
-      radius: [10, '60%'],
+      radius: [8, '100%'],
       center: ['50%', '50%'],
       roseType: 'area',
       itemStyle: {
@@ -386,7 +386,7 @@ export const coverageData4=ref({
         { value: 26, name: '材料管理' },
       ],
       label: {
-        show: true,
+        show: false,
         formatter: function(params) {
           return `{b|${params.name}}\n{c|${params.value}}`;
         },
@@ -404,8 +404,104 @@ export const coverageData4=ref({
         }
       },
       labelLine: {
-        show: true // 显示引导线
+        show: false // 显示引导线
       }
     }
   ]
 })
+
+export const coverageData5 = ref( {
+  color:[''],
+  color:['rgba(9,161,234,0.7)'],
+  grid: {
+    left: '1%',
+    right: '1%',
+    bottom: '1%',
+    top: '1%',
+    containLabel: true
+  },
+  title: {
+      show:false,
+      text: '甘特图测试',
+      x: '甘特图测试'
+  },
+
+  yAxis: [{
+    axisLabel:{
+      color:"#fff"
+    },
+      type: "category",
+      data: ["土石方开挖", "基础施工", "地下室主体施工", "地上主体结构施工", "抹灰施工", "屋面施工"]
+  }],
+  xAxis: [{
+      type: 'value',
+      axisLabel: {
+        color:"#fff",
+          formatter: function(value) {
+              var end_time = 1718428800000;
+              var start_time = 1717392000000;
+              var date = [];
+              var i = 1;
+              while (end_time > start_time) {
+                  var date_formatter = new Date(start_time);
+                  var date_time = (date_formatter.getFullYear() + "-" + (date_formatter.getMonth() + 1) + "-" + date_formatter.getDate());
+                  date.push(date_time);
+                  start_time = start_time + i * 24 * 60 * 60 * 1000;
+              }
+            //   console.log(date);
+              return date[value * 1];
+          }
+      },
+  }],
+  series: [{
+          name: "土石方开挖",
+          type: "bar",
+          stack: "总",
+          barWidth: 20,
+          itemStyle: {
+              normal: {
+                  barBorderColor: 'rgba(0,0,0,0)',
+                  color: 'rgba(255,255,255,0.1)'
+              }
+          },
+          data: [1, 1, 2, 3, 4, 5, 6]
+      },
+      {
+          name: "基础施工",
+          type: "bar",
+          barWidth: 20,
+          stack: "总",
+          data: [3],
+      },
+      {
+          name: "地下室主体施工",
+          type: "bar",
+          barWidth: 20,
+          stack: "总",
+          data: [0, 4, 0, 0, 0, 0, 0],
+      },
+      {
+          name: "地上主体结构施工",
+          type: "bar",
+          barWidth: 20,
+          stack: "总",
+          data: [0, 0, 2, 0, 0, 0, 0],
+      },
+      {
+          name: "抹灰施工",
+          type: "bar",
+          barWidth: 20,
+          stack: "总",
+          data: [0, 0, 0, 5, 0, 0, 0],
+      },
+      {
+          name: "屋面施工",
+          type: "bar",
+          barWidth: 20,
+          stack: "总",
+          data: [0, 0, 0, 0, 4, 0, 0],
+          
+      },
+      
+  ]
+});
--- src/view/home/detail/components/rightContent/index.vue
+++ src/view/home/detail/components/rightContent/index.vue
@@ -1,65 +1,195 @@
 <template>
   <div class="">
-    <div>
-      <PubTit style="width: 870px" :title="'人员管控'" :width="1088"></PubTit>
-      <div class="mt-[20px] flex">
-        <img src="@/assets/rou/icon57.png"  draggable="false" class="mr-[50px]" alt="" />
-        <div
-          class="w-[40%] h-[180px] flex flex-col items-center justify-center"
-        >
-          <!-- <div class="text-[20px] mb-[20px] is-title-bg text-[#fff]">特殊工种分布</div> -->
-          <!-- <EcharsWrapper :chartData="coverageData"></EcharsWrapper> -->
-           <img src="@/assets/rou/icon641.png" alt="">
-        </div>
-      </div>
-    </div>
-    <div>
-      <PubTit
-        style="width: 670px"
-        :title="'安全监管'"
-        :width="1088"
-      ></PubTit>
-      <div class="flex items-center">
-      <img src="@/assets/rou/icon58.png" class="ml-[50px] w-[330px] h-[212px]" alt="">
-        <EcharsWrapper class="w-[350px] h-[250px]" :chartData="coverageData4"></EcharsWrapper>
-        <img src="@/assets/rou/icon642.png"  draggable="false" alt="">
-      </div>
-
-    </div>
-    <div>
-      <PubTit style="width: 670px" :title="'质量监测'" :width="1088"></PubTit>
-      <div class="flex text-[#fff]">
-        <div
-          class="w-[50%] h-[350px] flex flex-col items-center justify-center"
-        >
-          <div class="w-[100%] flex justify-center flex-col items-center">
-            <div class="text-[18px] is-title-bg mt-[10px] mb-[10px]">质量问题情况</div>
-            <div class="grid grid-cols-4 grid-rows-1 gap-[10px] w-[100%] ">
-              <div class="flex flex-col relative after:absolute after:right-[30px] after:top-[12.5%] after:block after:content-[''] after:w-[1px] after:h-[75%] after:bg-[rgba(110,181,233,0.3)]"  v-for="(item,index) in listData">
-                <div class="flex items-end">
-                  <span class="text-[30px]">{{item.value}}</span>
-                  <span class="text-[18px] ml-[5px]">个</span>
+    <div class="flex height-[56%]">
+      <div>
+        <PubTit style="width: 555px" :title="'质量监测'" :width="555"></PubTit>
+        <div>
+          <div class="flex text-[#fff]">
+            <div class="w-[100%] flex flex-col items-center justify-center">
+              <div class="w-[100%] flex justify-center flex-col items-center">
+                <div class="text-[18px] is-title-bg mt-[10px] mb-[10px]">
+                  质量问题情况
                 </div>
-                <span class="text-[18px] mt-[5px]">{{item.name}}</span>
+                <div class="grid grid-cols-4 grid-rows-1 gap-[10px] w-[100%]">
+                  <div
+                    class="flex flex-col relative after:absolute after:right-[30px] after:top-[12.5%] after:block after:content-[''] after:w-[1px] after:h-[75%] after:bg-[rgba(110,181,233,0.3)]"
+                    v-for="(item, index) in listData"
+                  >
+                    <div class="flex items-end">
+                      <span class="text-[30px] tit-2">{{ item.value }}</span>
+                      <span class="text-[18px] tit-2 ml-[5px]">个</span>
+                    </div>
+                    <span class="text-[18px] mt-[5px]">{{ item.name }}</span>
+                  </div>
+                  <div class="flex flex-col items-center">
+                    <div class="flex items-end text-[rgba(86,209,146,1)]">
+                      <span class="text-[30px]">99.43</span>
+                      <span class="text-[18px] ml-[5px]">%</span>
+                    </div>
+                    <span class="text-[18px] mt-[5px]">整改率</span>
+                  </div>
+                </div>
               </div>
-              <div class="flex flex-col items-center">
-                <div class="flex items-end text-[rgba(86,209,146,1)]">
-                  <span class="text-[30px]">99.43</span>
-                  <span class="text-[18px] ml-[5px]">%</span>
-                </div>
-                <span class="text-[18px] mt-[5px]">整改率</span>
+              <div class="h-[260px]">
+                <EcharsWrapper
+                  :chartData="coverageData3"
+                ></EcharsWrapper>
+              </div>
+              <div class="h-[78px]">
+                <img src="@/assets/sxk-qph/bhgs.png" draggable="false" alt="" />
               </div>
             </div>
           </div>
-          <EcharsWrapper :chartData="coverageData3"></EcharsWrapper>
         </div>
-        <div class="flex">
-          <img  draggable="false" src="@/assets/rou/icon643.png" alt="">
-          <img  draggable="false" src="@/assets/rou/icon644.png" alt="">
+      </div>
+      <div>
+        <PubTit style="width: 555px" :title="'安全监测'" :width="555"></PubTit>
+        <div class="mt-[20px] mr-[29px]">
+          <img
+            src="@/assets/sxk-qph/qnjc.png"
+            draggable="false"
+            class="mr-[50px] h-[205px]"
+            alt=""
+          />
+          <div>
+            <div class="flex text-[#fff]">
+              <div class="w-[100%] flex flex-col items-center justify-center h-[255px]">
+                <div class="w-[100%] flex justify-center flex-col items-center">
+                  <div class="text-[18px] is-title-bg mt-[10px] mb-[10px]">
+                    安全隐患
+                  </div>
+                  <div class="grid grid-cols-4 grid-rows-1 gap-[10px] w-[100%]">
+                    <div
+                      class="flex flex-col relative after:absolute after:right-[30px] after:top-[12.5%] after:block after:content-[''] after:w-[1px] after:h-[75%] after:bg-[rgba(110,181,233,0.3)]"
+                      v-for="(item, index) in listData1"
+                    >
+                      <div class="flex items-end">
+                        <span class="text-[30px] tit-2">{{ item.value }}</span>
+                        <span class="text-[18px] tit-2 ml-[5px]">个</span>
+                      </div>
+                      <span class="text-[18px] mt-[5px]">{{ item.name }}</span>
+                    </div>
+                    <div class="flex flex-col items-center">
+                      <div class="flex items-end text-[rgba(86,209,146,1)]">
+                        <span class="text-[30px]">97.14</span>
+                        <span class="text-[18px] ml-[5px]">%</span>
+                      </div>
+                      <span class="text-[18px] mt-[5px]">整改率</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="flex h-[166px]">
+                  <EcharsWrapper
+                    class="w-[110px] h-[166px]"
+                    :chartData="coverageData4"
+                  ></EcharsWrapper>
+                  <div>
+                    <div class="mt-[10px]">
+                      <div class="flex mt-[35px]">
+                        <div class="flex mt-[10px] ml-[30px]">
+                          <div class="flex justify-center items-center">
+                            <div class="text-[#BAC3C4] text-[17px] is-dot">
+                              材料管理
+                            </div>
+                            <div class="text-[17px] color-[#488CF7] ml-2">
+                              77
+                            </div>
+                          </div>
+                        </div>
+                        <div class="flex mt-[10px] ml-[30px]">
+                          <div class="flex justify-center items-center">
+                            <div class="text-[#BAC3C4] text-[17px] is-dot">
+                              现场防火
+                            </div>
+                            <div class="text-[17px] color-[#488CF7] ml-2">
+                              36
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="flex mt-[21px]">
+                        <div class="flex mt-[10px] ml-[30px]">
+                          <div class="flex justify-center items-center">
+                            <div class="text-[#BAC3C4] text-[17px] is-dot">
+                              洞口防护
+                            </div>
+                            <div class="text-[17px] color-[#488CF7] ml-2">
+                              25
+                            </div>
+                          </div>
+                        </div>
+                        <div class="flex mt-[10px] ml-[30px]">
+                          <div class="flex justify-center items-center">
+                            <div class="text-[#BAC3C4] text-[17px] is-dot">
+                              扣件式钢管脚手架
+                            </div>
+                            <div class="text-[17px] color-[#488CF7] ml-2">
+                              20
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="flex mt-[21px]">
+                        <div class="flex mt-[10px] ml-[30px]">
+                          <div class="flex justify-center items-center">
+                            <div class="text-[#BAC3C4] text-[17px] is-dot">
+                              临边防护
+                            </div>
+                            <div class="text-[17px] color-[#488CF7] ml-2">
+                              16
+                            </div>
+                          </div>
+                        </div>
+                        <div class="flex mt-[10px] ml-[30px]">
+                          <div class="flex justify-center items-center">
+                            <div class="text-[#BAC3C4] text-[17px] is-dot">
+                              未分类
+                            </div>
+                            <div class="text-[17px] color-[#488CF7] ml-2">
+                              15
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    
+    <div class="flex height-[54%]">
+      <div>
+        <PubTit style="width: 555px" :title="'施工进度'" :width="555"></PubTit>
+        <div>
+          <div class="flex justify-center mt-[18px]">
+            <div class="flex">
+              <div v-for="item in KeyProgress" :key="item" class="w-[100px] h-[50px] flex items-center justify-center key-progress-item">
+                <div class="key-progress">
+                  {{ item }}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="h-[276px] w-[100%] mt-[31px]">
+            <EcharsWrapper :chartData="coverageData5"></EcharsWrapper>
+          </div>
+        </div>
+      </div>
+      <div>
+        <PubTit style="width: 555px" :title="'项目支付情况'" :width="555"></PubTit>
+        <div class="flex items-center justify-center m-[20px] ml-[70px] ">
+         <img
+            src="@/assets/sxk-qph/ty.png"
+            draggable="false"
+            class="w-[420px] h-[346px]"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -67,8 +197,9 @@
 import { ref } from "vue";
 import PubTit from "@/components/public/PubTit.vue";
 import EcharsWrapper from "@/components/echars/EcharsWrapper.vue";
-import { coverageData,coverageData3,coverageData4 } from "./echarsOptions.js";
+import { coverageData, coverageData3, coverageData4,coverageData5 } from "./echarsOptions.js";
 import SeamlessScroll from "@/components/SeamlessScroll.vue";
+const KeyProgress = ['关键进度一', '关键进度二', '关键进度三', '关键进度四'];
 const scrollTableList = ref([
   {
     a: "1",
@@ -104,7 +235,21 @@ const listData = ref([
   {
     name: "已整改",
     value: 348,
-  }
+  },
+]);
+const listData1 = ref([
+  {
+    name: "隐患总数",
+    value: 315,
+  },
+  {
+    name: "重大隐患",
+    value: 0,
+  },
+  {
+    name: "已整改",
+    value: 306,
+  },
 ]);
 const listData2 = ref([
   {
@@ -118,7 +263,7 @@ const listData2 = ref([
   {
     name: "已整改",
     value: 306,
-  }
+  },
 ]);
 </script>
 
@@ -130,5 +275,109 @@ const listData2 = ref([
   justify-content: center;
   align-items: center;
 }
+.title-box {
+  // font-weight: bold;
+  // font-size: 24px;
+  // color: #f6f9fe;
+  // text-shadow: 0px 2px 8px rgba(5, 28, 55, 0.42);
+  width: 132px;
+  height: 29px;
+  background: linear-gradient(
+    0deg,
+    rgba(44, 96, 140, 0.92) 0%,
+    rgba(55, 177, 255, 0.77) 100%
+  );
+  border-radius: 1px;
+  border: 1px solid;
+  opacity: 0.58;
+  border-image: linear-gradient(0deg, #79caf0, #06baff, #79caf0) 10 10;
+}
+.title-text {
+  width: 100%;
+  // height: 15px;
+  font-family: Alibaba PuHuiTi;
+  font-weight: 400;
+  color: #83d9ff;
+  background: linear-gradient(
+    0deg,
+    rgba(107, 182, 237, 0.96) 0%,
+    rgba(255, 255, 255, 0.96) 82.6904296875%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.bt-title-bg2 {
+  background: linear-gradient(
+    0deg,
+    rgba(44, 96, 140, 0.92) 0%,
+    rgba(55, 177, 255, 0.77) 100%
+  );
+  border-radius: 1px;
+  border-top: 1px solid;
+  border-bottom: 1px solid;
+  opacity: 0.58;
+  border-image: linear-gradient(0deg, #79caf0, #06baff, #79caf0) 10 10;
+}
+.tit-2 {
+  font-family: Source Han Sans CN;
+  font-weight: bold;
+  // font-size: 24px;
+  color: #50bbff;
+  // line-height: 48px;
+  background: linear-gradient(0deg, #ffffff 0%, #6bb6ed 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.tit1 {
+  font-family: Source Han Sans CN;
+  font-weight: bold;
+  font-size: 24px;
+  color: #17b99a;
+  background: linear-gradient(0deg, #eefdff 0%, #6bffac 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.border-bg-op {
+  background: linear-gradient(
+    0deg,
+    rgba(21, 171, 250, 0) 0%,
+    rgba(109, 199, 246, 0.78) 50%,
+    rgba(21, 171, 250, 0) 100%
+  );
+  opacity: 0.4;
+}
+.is-dot {
+  position: relative;
+  padding-left: 20px;
+}
+.is-dot::before {
+  display: block;
+  content: "";
+  width: 10px;
+  height: 10px;
+  background: linear-gradient(0deg, #488cf7 0%, #ffffff 100%);
+  border-radius: 50%;
+  margin-right: 10px;
+  position: absolute;
+  left: 0px;
+  top: calc(50% - 5px);
+}
+.key-progress {
+  width: 76px;
+height: 13px;
+font-family: YouSheBiaoTiHei;
+font-weight: 400;
+font-size: 16px;
+color: #FFFFFF;
+background: linear-gradient(0deg, #79C5FF 0%, #FFFFFF 59.8388671875%, #FFFFFF 100%);
+-webkit-background-clip: text;
+-webkit-text-fill-color: transparent;
+}
+.key-progress-item {
+    // background: rgba(255, 255, 255, 0.1);
+    // border: 1px solid rgba(255, 255, 255, 0.2);
+  // background: radial-gradient(circle, rgba(0, 0, 0, 0.5), rgba(255, 255, 255, 0.1));
+  background: radial-gradient(circle, rgb(1, 19, 42,0.1), rgba(255, 255, 255, 0.3));
 
+}
 </style>
